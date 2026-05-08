66260efa51f0f0989ddd1c4a2635ba8cd7b2a108
steve <steve@ridgeroad.duckdns.org>
2026-05-08 15:32:59 -0400

    helpers/config.helpers.js

--- helpers/config.helpers.js
+++ helpers/config.helpers.js
@@ -1,7 +1,32 @@
 import { Configurator } from '@cityssm/configurator';
 import { secondsToMillis } from '@cityssm/to-millis';
-import { config } from '../data/config.js';
+import config from '../data/config.js';
 import { configDefaultValues } from './config.defaults.js';
+
+const loginUsername = process.env.LOGIN_USERNAME;
+const loginPassword = process.env.LOGIN_PASSWORD;
+
+if (loginUsername && loginPassword) {
+  config.login = {
+    authentication: {
+      config: {
+        authenticate: (userName, password) => {
+          const cleanUserName = userName.replace(/^\\/, '');
+          return cleanUserName === loginUsername && password === loginPassword;
+        }
+      },
+      type: 'function'
+    },
+    domain: ''
+  };
+  config.users = {
+    canLogin: [loginUsername],
+    canUpdate: [loginUsername],
+    isAdmin: [loginUsername],
+    testing: []
+  };
+}
+
 const configurator = new Configurator(configDefaultValues, config);
 export function getConfigProperty(propertyName, fallbackValue) {
     return configurator.getConfigProperty(propertyName, fallbackValue);
